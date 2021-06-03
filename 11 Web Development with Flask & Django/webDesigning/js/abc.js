7e8ff007695b4cf270be45aabed77167bf1c023f
qasim1020 <qasimhassan1020@gmail.com>
2021-06-03 00:01:44 -1200

    Web development with Flask & Django

---
+++ 11 Web Development with Flask & Django/webDesigning/js/abc.js
@@ -0,0 +1,9 @@
+alert("Hello");
+var uname= prompt("Enter your email address");
+document.write("Hello World with JavaScript <br> Hello <b>"+uname+"</b>");
+console.log("ABC"+uname);
+
+function abc(){
+    alert("This Message from abc function");
+    document.getElementById("msg").innerText = "Pakistan zinda bad!";
+}
